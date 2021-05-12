f85cfc0ee748b03c7d9bfed80fdfd885ee14d7e7
huobimoon <jemeriaantonios41@gmail.com>
2021-05-12 13:33:33 +0800

    first commit

---
+++ src/apollo/client.js
@@ -0,0 +1,42 @@
+import { ApolloClient } from 'apollo-client'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { HttpLink } from 'apollo-link-http'
+
+export const client = new ApolloClient({
+  link: new HttpLink({
+    uri: 'https://q.hg.network/subgraphs/name/pippiswap/heco',
+  }),
+  cache: new InMemoryCache(),
+  shouldBatch: true,
+})
+
+export const healthClient = new ApolloClient({
+  link: new HttpLink({
+    uri: 'https://h.hg.network/graphql',
+  }),
+  cache: new InMemoryCache(),
+  shouldBatch: true,
+})
+
+export const v1Client = new ApolloClient({
+  link: new HttpLink({
+    uri: 'https://q.hg.network/subgraphs/name/pippiswap/heco',
+  }),
+  cache: new InMemoryCache(),
+  shouldBatch: true,
+})
+
+export const stakingClient = new ApolloClient({
+  link: new HttpLink({
+    uri: 'https://q.hg.network/subgraphs/name/pippiswap/heco',
+  }),
+  cache: new InMemoryCache(),
+  shouldBatch: true,
+})
+
+export const blockClient = new ApolloClient({
+  link: new HttpLink({
+    uri: 'https://q.hg.network/subgraphs/name/kdex-heco-blocks',
+  }),
+  cache: new InMemoryCache(),
+})
---
+++ src/components/Attribution/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Attribution = () => (
+  <p className="attribution">
+    <a href="https://swap.pippi.finance/" rel="noopener noreferrer" target="_blank">
+      Pippiswap
+    </a>{' '}
+  </p>
+)
+
+export default Attribution
